724a2abbc816c8facc99819120c34ccfca68e252
Massimiliano <massimiliano@MacBook-Pro-di-Massimiliano.local>
2019-06-15 14:10:57 +0100

    quasi finito

--- newsAlamofire/Controller/NewsVC.swift
+++ newsAlamofire/Controller/NewsVC.swift
@@ -9,22 +9,26 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import SafariServices
 
 
 
 
 class NewsVC: UIViewController {
-
+    var nomeCitta : String?
+    var inizialeCitta : String?
     let APP_ID = "d1100591b9054c3da2fef23e4c9a2a15"
-    let MY_URL = "https://newsapi.org/v2/top-headlines?country=it"
-   // let params : [String : String] = ["apiKey" : APP_ID]
+    let MY_URL = "https://newsapi.org/v2/top-headlines?country="
+   
     
     var count = 0
     @IBOutlet weak var newsCollection: UICollectionView!
     
     
+    @IBOutlet weak var cityTitleLbl: UILabel!
     var arrayTitle : [String] = []
     var arrayImage : [String] = []
+    var arrayURL : [String] = []
     let news = News()
     
    
@@ -32,22 +36,26 @@ class NewsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("cazzoooooo",inizialeCitta!)
         newsCollection.delegate = self
         newsCollection.dataSource = self
+        cityTitleLbl.text = nomeCitta
         startJSON()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         newsCollection.reloadData()
+       // startJSON()
     }
 let new = "https://newsapi.org/v2/top-headlines?country=us&=d1100591b9054c3da2fef23e4c9a2a15"
   let old = "https://newsapi.org/v2/top-headlines?country=us&apiKey=d1100591b9054c3da2fef23e4c9a2a15"
     
     
     func startJSON (){
-        let params : [String : String] = ["apiKey" : APP_ID]
-        getWeatherData(url: MY_URL, parameters: params)
+       let params : [String : String] = ["apiKey" : APP_ID]
+        getWeatherData(url: MY_URL + inizialeCitta! , parameters: params)
     }
     
     
@@ -55,6 +63,7 @@ let new = "https://newsapi.org/v2/top-headlines?country=us&=d1100591b9054c3da2fe
         Alamofire.request(url, method: .get, parameters:parameters).responseJSON{
             response in
             if response.result.isSuccess{
+                print(response.request?.description)
                 print("Success! Got the weather data")
                 let newsJSON : JSON = JSON(response.result.value!)
                 self.updateWeatherData(json: newsJSON)
@@ -72,8 +81,10 @@ let new = "https://newsapi.org/v2/top-headlines?country=us&=d1100591b9054c3da2fe
         for i in 0...19 {
              var image = json["articles"][i]["urlToImage"].stringValue
              var title = json["articles"][i]["title"].stringValue
+             var url = json["articles"][i]["url"].stringValue
              arrayTitle.append(title)
              arrayImage.append(image)
+             arrayURL.append(url)
         }
         var author = json["articles"][0]["author"].stringValue
         
@@ -92,6 +103,19 @@ let new = "https://newsapi.org/v2/top-headlines?country=us&=d1100591b9054c3da2fe
         self.newsCollection.reloadData()
 
     }
+    
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func showSafariVC(url : String){
+    guard let url = URL(string : url) else { return }
+    
+    let safariVC = SFSafariViewController(url: url)
+    present(safariVC, animated: true)
+    }
+    
 }
 extension NewsVC : UICollectionViewDelegate, UICollectionViewDataSource {
     
@@ -108,6 +132,11 @@ extension NewsVC : UICollectionViewDelegate, UICollectionViewDataSource {
         cell?.setupTable(title : indexTitle, image: indexImage)
         return cell!
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        showSafariVC(url: arrayURL[indexPath.row])
+    }
+    
     
     
 }
